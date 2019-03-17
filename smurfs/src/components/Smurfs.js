616f629b9e3800b4e8910b177e63aef1da14a6fc
tactnician <tactnician@gmail.com>
2019-03-17 11:18:40 -0400

    added Smurf and SmurfForm components

---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Smurfs extends Component {
+    render() {
+        return (
+        <div>
+            
+        </div>
+        )
+    }
+}
